dc2511bd30d2ad7daa7c046152ac3e8a75c53574
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-08-07 20:13:54 +0000

    creates Server.cpp

--- include/Server.hpp
+++ include/Server.hpp
@@ -3,12 +3,56 @@
 #define SERVER_HPP 1
 
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 using namespace std;
 
 namespace Server
 {
+    class ServerInterface
+    {
+        public:
 
+            virtual int create() = 0;
+            virtual void setConfig() = 0;
+            virtual void sendResponse(char* buffer) = 0;
+            virtual char* read() = 0;
+            virtual void start() = 0;
+            virtual void closeSocket() = 0;
+
+    };
+    class ServerHandler : public ServerInterface 
+    {
+        int fd, port, opt, newSocket;
+        struct sockaddr_in address;
+        socklen_t addrlen;
+
+        public:
+
+            ServerHandler(int port, int opt) : port(port), opt(opt)
+            {
+
+            }
+            virtual ~ServerHandler() = default;
+
+            ServerHandler operator=(const ServerHandler& s);
+
+            void operator()(int o);
+
+        private:
+
+            virtual int create() override; 
+            virtual void setConfig() override;
+
+        public:
+
+            virtual void sendResponse(char *buffer) override;
+            virtual char* read() override;
+            virtual void start() override;
+            virtual void closeSocket() override;
+
+    };
 
 };
 
---
+++ src/Server.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "../include/Server.hpp"
+#include <cstring>
+#include <unistd.h>
+
+using namespace std;
+using namespace Server;
+
+ServerHandler ServerHandler::operator=(const ServerHandler& s)
+{
+
+    return ServerHandler(8080, 1);
+}
+void ServerHandler::operator()(int o)
+{
+    int a = 5;
+    int b = 4;
+
+    auto f = [&a, &b](int *te) -> int
+    {
+        *te = 5;
+
+        te++;
+        
+        return *te;
+    };
+
+}
+int ServerHandler::create()
+{
+    //creates new socket as fd
+
+    this->fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    return this->fd;
+}
+void ServerHandler::setConfig()
+{ 
+    if(setsockopt(this->fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &this->opt, sizeof(this->opt)))
+        throw string("Set options error");
+
+    //set server config
+
+    this->address.sin_family = AF_INET;
+    this->address.sin_addr.s_addr = INADDR_ANY;
+    this->address.sin_port = htons(this->port);
+
+    this->addrlen = sizeof(this->address);
+
+    //set port for server
+
+    if(bind(this->fd, (struct sockaddr *)&this->address, sizeof(this->address)) < 0)
+        throw string("Bind set error");
+
+    //start listening server
+
+    if(listen(this->fd, 3) < 0)
+        throw string("Listen error");
+}
+void ServerHandler::sendResponse(char* buffer)
+{
+    //sending request to client
+
+    send(this->newSocket, buffer, strlen(buffer), 0);
+
+}
+char* ServerHandler::read()
+{
+
+}
+void ServerHandler::start()
+{
+    this->newSocket = accept(this->fd, (struct sockaddr *)&this->address, &addrlen);
+
+    if(this->newSocket < 0)
+        throw string("Accept error");
+
+}
+void ServerHandler::closeSocket()
+{
+    //close newSocket and fd sockets
+
+   close(this->newSocket);
+   close(this->fd);
+
+}
